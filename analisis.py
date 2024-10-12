1dcdaeb134bff90de4a8c7239aad21a31066c13d
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-10-12 15:53:40 -0600

    Se separa la comparativa entre la fórmula obtenida y los modelos de la variable_2 a otra función. Se agrega el archivo .ggb para simular la fórmula obtenida en geogebra.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- analisis.py
+++ analisis.py
@@ -47,7 +47,42 @@ def modelosVar1():
 
 
 #   Encontrar modelos de variable_2
+
+
 def modelosVar2():
+    max_x = 20
+
+    # Se obtienen los datos de la variable_2
+    variable_2 = data["variable_2"]
+    params = expon.fit(variable_2)
+    x = np.linspace(variable_2.min(), max_x, 200)
+
+    # Se obtienen los ajustes
+    pdf = expon.pdf(x, *params)
+    cdf = expon.cdf(x, *params)
+
+    # Se arma una figura con 2 gráficos
+    plt.figure(figsize=(10, 5))
+
+    # Mostrar PDF
+    plt.subplot(1, 2, 1)
+    plt.plot(x, pdf, 'r-', lw=2, label='PDF')
+    plt.title('Función de densidad de probabilidad')
+    plt.xlabel('Variable_2')
+    plt.ylabel('Probabilidad')
+
+    # Mostrar CDF
+    plt.subplot(1, 2, 2)
+    plt.plot(x, cdf, 'b-', lw=2, label='CDF')
+    plt.title('Función de probabilidad acumulativa')
+    plt.xlabel('Variable_2')
+    plt.ylabel('Probabilidad acumulativa')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def comparacionFormula():
     max_x = 20
 
     # Se obtienen los datos de la variable_2
