e02287a913cc585d0e95e2d31ca166ea571b9383
Bryan Cortes <bryanco0310@gmail.com>
2024-11-17 17:17:33 -0600

    Modificacion y recolecion de datos
    Signed-off-by: Bryan Cortes <bryanco0310@gmail.com>

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import configparser
-
+from datetime import datetime
 
 # Crear la clase base de la tabla
 class Base(DeclarativeBase):
@@ -17,22 +17,37 @@ if db == "sqlite":
 elif db == "postgresql":
     system = config["db"]["postgresql"]
 
-
-# Definir los modelos
+# Definir el modelo TestData
 class TestData(Base):
-    __tablename__ = "test_data"
+    __tablename__ = "data_proyecto"
 
     id = Column(Integer, primary_key=True)
-    group = Column(String)
-    timestamp = Column(DateTime)
-    variable_1 = Column(Integer)
-    variable_2 = Column(Float)
+    group = Column(String, nullable=False)           # Campo obligatorio
+    timestamp = Column(DateTime, nullable=False)      # Campo obligatorio
+    sunlight = Column(Boolean, nullable=False)        # Indica si hay luz solar
+    value = Column(Float, nullable=False)             # Almacena cada muestra de datos
+    minutes = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return (f"<TestData(id={self.id}, group={self.group}, timestamp={self.timestamp}, "
+                f"sunlight={self.sunlight}, value={self.value}),  minutes={self.minutes})>")
+       # Calcula los minutos automáticamente al asignar el timestamp
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.timestamp:
+            self.minutes = self.calculate_minutes(self.timestamp)
+
+    @staticmethod
+    def calculate_minutes(timestamp: datetime) -> int:
+        """Calcula los minutos desde la medianoche"""
+        return timestamp.hour * 60 + timestamp.minute
 
 
-# Crear la conexión a la base de datos SQLite3 o PostgreSQL
+
+# Crear la conexión a la base de datos (SQLite o PostgreSQL)
 engine = create_engine(system)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Crear la(s) tabla(s) en la base de datos
+# Crear las tablas en la base de datos
 Base.metadata.create_all(engine)
