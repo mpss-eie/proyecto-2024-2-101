3169421187895e62d306a13793147ba04fe512a5
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-11-21 17:34:41 -0600

    Se realizan las pruebas de fitting para la distribución general.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- src/analisisParte2.py
+++ src/analisisParte2.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import sqlite3
 from fitter import Fitter
-import matplotlib
 import matplotlib.pyplot as plt
-# import numpy as np
-# from scipy.stats import norm, expon
-# from scipy.integrate import cumulative_trapezoid
-
-matplotlib.use('TkAgg')
+from collections import Counter
+from scipy.stats import norm
 
 # Extraer los datos a usar.
 conn = sqlite3.connect("proyecto.db")
@@ -25,58 +21,75 @@ distributions = [
 ]
 
 
+def obtenerMejorFit(data):
+    valores = data['value']
+
+    # Iniciar contadores de cantidad de veces que
+    # aparece una distribución como la mejor, general y según si es día
+    contador_general = Counter()
+    contador_dia = Counter()
+    contador_noche = Counter()
+
+    # Obtener el mejor fit de cada instante de tiempo
+    x = 0
+    for _ in range(max(data['minutes'].values) - 1):
+        # Valores y estado de sunlight del instante
+        instante = valores.iloc[x:x + 100]
+        sunlight = data['sunlight'].iloc[x]
+
+        # Hacer el fit
+        f = Fitter(instante.values, distributions=distributions)
+        f.fit()
+
+        # Agregar a contadores
+        contador_general[next(iter(f.get_best()))] += 1
+
+        if sunlight:
+            contador_dia[next(iter(f.get_best()))] += 1
+        else:
+            contador_noche[next(iter(f.get_best()))] += 1
+        x += 100
+
+    print('\nContador general:')
+    print(contador_general)
+
+    print('\nContador dia:')
+    print(contador_dia)
+
+    print('\nContador noche:')
+    print(contador_noche)
+
+
 def calcularPromedios(data):
     # Calcular el promedio y agregarlo al dataFrame
-    data['promedio'] = data.groupby('minutes')['value'].transform('mean')
+    data['average'] = data.groupby('minutes')['value'].transform('mean')
 
     # Ordenar el dataFrame
     data = data.sort_values(by=['minutes', 'id']).reset_index(drop=True)
 
-    print(data[data['minutes'] == 0])
+    # Mostrar figura de los promedios vs minutos
+    eje_minutos = data[['minutes', 'average']].drop_duplicates()
 
-    print(data)
+    # Plot
+    plt.figure(figsize=(8, 5))
+    plt.plot(eje_minutos['minutes'], eje_minutos['average'],
+             marker='o', linestyle='-')
+    # sns.lineplot(data=eje_minutos, x='minutes', y='average', marker='o')
+    plt.title('Promedio de proceso en función del tiempo.')
+    plt.xlabel('Tiempo (Minutos)')
+    plt.ylabel('Promedio')
+    plt.grid()
+    plt.show()
 
     return data
 
 
-def histogramaInstante(df):
-    """
-    Genera
-    """
-    # Crear el histograma
-    plt.figure()
-    plt.hist(df, density=True, bins=100, edgecolor='black')
-    plt.title('Histograma de valor en cierto instante.')
-    plt.xlabel('Valores de variable')
-    plt.ylabel('densidad')
-    # Guardar el gráfico como un archivo .png
-    plt.show()
+def calcularParametros(data):
+    # Se obtuvo un mejor ajuste de tipo: Normal
 
-
-def obtenerFuncionDensidad():
-    valores = data['value']
-
-    # Datos correspondientes a un mismo instante de tiempo
-    instante1 = valores.iloc[0:100]
-    f_1 = Fitter(instante1.values, distributions=distributions)
-    f_1.fit()
-    # f_1.summary()
-
-    instante2 = valores.iloc[100:200]
-    f_2 = Fitter(instante2.values, distributions=distributions)
-    f_2.fit()
-    # f_2.get_best()
-
-    instante3 = valores.iloc[200:300]
-    f_3 = Fitter(instante3.values, distributions=distributions)
-    f_3.fit()
-    # f_3.get_best()
-
-    f_2.summary()
-    plt.show()
-    histogramaInstante(instante2)
+    pass
 
 
 if __name__ == "__main__":
-    # obtenerFuncionDensidad()
-    data = calcularPromedios(data)
+    obtenerMejorFit(data)
+    # data = calcularPromedios(data)
